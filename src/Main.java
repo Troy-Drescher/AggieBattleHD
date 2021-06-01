9dac765054f6fe2674efbd2b992a1dcce03afb66
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-06-01 16:49:57 -0700

    added Mr. Ivy

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,8 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
   public ImageIcon ag5Icon1 = new ImageIcon(cl.getResource("FinalityIcon.png"));
   public ImageIcon ag6Icon1 = new ImageIcon(cl.getResource("CharChimpIcon.png"));
   public ImageIcon ag7Icon1 = new ImageIcon(cl.getResource("StrangeCharmIcon.png"));
-  static ImageIcon ag1Icon,ag2Icon,ag3Icon,ag4Icon,ag5Icon,ag6Icon,ag7Icon;
+  public ImageIcon ag8Icon1 = new ImageIcon(cl.getResource("CorvakaIcon.png"));
+  static ImageIcon ag1Icon,ag2Icon,ag3Icon,ag4Icon,ag5Icon,ag6Icon,ag7Icon,ag8Icon;
 /*
   private ImageIcon ag7Icon = new ImageIcon("SportBlack.jpeg");
   private ImageIcon ag8Icon = new ImageIcon("SportBlue.jpeg");
@@ -77,6 +78,7 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
             Main.ag5Icon=ag5Icon1;
             Main.ag6Icon=ag6Icon1;
             Main.ag7Icon=ag7Icon1;
+            Main.ag8Icon=ag8Icon1;
         }
 	public Main(Aggie player1, Aggie player2) {
 		player1.setXpos(250);
@@ -179,7 +181,8 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
     Aggie a5 = new Iceguin(new Sprite("Iceguin1.png"),"Iceguin",125, 40, 35, 75,ag4Icon);
     Aggie a6 = new Pegasus(new Sprite("Pegasus1.png"),"Pegasus",70,50,70,80,ag3Icon);
     Aggie a7 = new StrangeCharm(new Sprite("StrangeCharm1.png"),"StrangeCharm",80,60,60,75,ag7Icon);
-		Aggie [] aggieList = {a1,a2,a3,a4,a5,a6,a7};
+    Aggie a8 = new Corvaka(new Sprite("Corvaka1.png"),"Corvaka",45,60,70,100,ag8Icon);
+		Aggie [] aggieList = {a1,a2,a3,a4,a5,a6,a7,a8};
 		AggieLists = aggieList;
 		AggieSelect2 as2 = new AggieSelect2();
 		AggieSelectListener asl = new AggieSelectListener();
